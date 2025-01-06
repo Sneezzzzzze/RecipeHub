4e7457d5450fd3b0e2bf90b60733db58ee69ec6b
Sneezzzzzze <kimjunbeom1205@gmail.com>
2025-01-06 14:05:27 +0700

    fix project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./ui/globals.css";
 import "@clayui/css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {geistMono, geistSans} from "@/app/ui/fonts";
 
 export default function RootLayout({
   children,
