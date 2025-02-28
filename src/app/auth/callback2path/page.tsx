0d039b2587f90af9da4fc1e649a839ffb9aade78
Sneezzzzzze <kimjunbeom1205@gmail.com>
2025-02-28 15:15:36 +0700

    fix error3

--- src/app/auth/callback2path/page.tsx
+++ src/app/auth/callback2path/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import Callback2Path from "./Callback2Path"; // 클라이언트 컴포넌트
+import Callback2Path from "@/app/auth/callback2path/Callback2Path"; // 클라이언트 컴포넌트
 import Loader from "@/app/ui/components/loading";
 
 export default function Page() {
